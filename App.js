47fe659f1931a011ece39562a8e5e4d137716bda
emre-cil <info.emrecil@gmail.com>
2022-08-15 00:02:05 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StackNavigator>
+        <Stack.Screen />
+        <Stack.Screen />
+      </StackNavigator>
+    </NavigationContainer>
+  );
+}
