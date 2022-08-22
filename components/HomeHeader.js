8c306425c20096a73fe6b014b92ca721eb3d946a
emre cil <info.emrecil@gmail.com>
2022-08-22 15:21:45 +0300

    performance update

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { View, Text, Image, TextInput } from 'react-native';
-
-import { COLORS, FONTS, SIZES, assets } from '../constants';
+import React, { useMemo } from 'react';
+import { View } from 'react-native';
+import { COLORS, SIZES } from '../constants';
+import InputArea from './InputArea';
+import HeaderDetails from './HeaderDetails';
 
 const HomeHeader = ({ onSearch }) => {
+  const headerDetails = useMemo(() => <HeaderDetails />, []);
   return (
     <View
       style={{
@@ -11,79 +13,8 @@ const HomeHeader = ({ onSearch }) => {
         padding: SIZES.font,
       }}
     >
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        <Image source={assets.logo} resizeMode="contain" style={{ width: 90, height: 25 }} />
-
-        <View style={{ width: 45, height: 45 }}>
-          <Image source={assets.person01} resizeMode="contain" style={{ width: '100%', height: '100%' }} />
-          <Image
-            source={assets.badge}
-            resizeMode="contain"
-            style={{
-              position: 'absolute',
-              width: 15,
-              height: 15,
-              bottom: 0,
-              right: 0,
-            }}
-          />
-        </View>
-      </View>
-
-      <View style={{ marginVertical: SIZES.font }}>
-        <Text
-          style={{
-            fontFamily: FONTS.regular,
-            fontSize: SIZES.small,
-            color: COLORS.white,
-          }}
-        >
-          Hello ProNFTGuyX11
-        </Text>
-
-        <Text
-          style={{
-            fontFamily: FONTS.bold,
-            fontSize: SIZES.large,
-            color: COLORS.white,
-            marginTop: SIZES.base / 2,
-          }}
-        >
-          Let’s find masterpiece Art
-        </Text>
-      </View>
-
-      <View style={{ marginTop: SIZES.font }}>
-        <View
-          style={{
-            width: '100%',
-            borderRadius: SIZES.font,
-            backgroundColor: COLORS.gray,
-            flexDirection: 'row',
-            alignItems: 'center',
-            paddingHorizontal: SIZES.font,
-            paddingVertical: SIZES.small - 2,
-          }}
-        >
-          <Image
-            source={assets.search}
-            resizeMode="contain"
-            style={{ width: 20, height: 20, marginRight: SIZES.base }}
-          />
-          <TextInput
-            placeholder="Search NFTs"
-            style={{ flex: 1, border: 0, outline: 0 }}
-            placeholderTextColor={COLORS.white}
-            onChangeText={onSearch}
-          />
-        </View>
-      </View>
+      {headerDetails}
+      <InputArea onSearch={onSearch} />
     </View>
   );
 };
